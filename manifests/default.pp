84ab79340f612e4833f120a1f0cce202630755a5
Micah Walter <micah@micahwalter.com>
2014-03-21 22:02:29 -0400

    puppet just installed apache -- woosh

---
+++ manifests/default.pp
@@ -0,0 +1,23 @@
+group { "puppet":
+	ensure => "present",
+}
+
+File { owner => 0, group => 0, mode => 0644 }
+
+file { '/etc/motd':
+	content => "Welcome to your Vagrant-built virtual machine! - Managed by Puppet.\n"
+}
+
+exec { "apt-get update":
+  path => "/usr/bin",
+}
+
+package { "apache2":
+  ensure  => present,
+  require => Exec["apt-get update"],
+}
+
+service { "apache2":
+  ensure  => "running",
+  require => Package["apache2"],
+}
