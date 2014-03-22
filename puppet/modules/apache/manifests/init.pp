c2beff848a5bf594d4e18077128f97accd2769e7
Micah Walter <micah@micahwalter.com>
2014-03-22 00:25:22 -0400

    configuring more apache and base with modules

---
+++ puppet/modules/apache/manifests/init.pp
@@ -0,0 +1,17 @@
+# == Class: apache
+#
+# Installs packages for Apache2, enables modules, and sets config files.
+#
+class apache {
+  package { ['apache2']:
+    ensure => present;
+  }
+
+  service { 'apache2':
+    ensure  => running,
+    require => Package['apache2'];
+  }
+
+  apache::conf { ['apache2.conf', 'envvars', 'ports.conf']: }
+  apache::module { ['rewrite.load']: }
+}
